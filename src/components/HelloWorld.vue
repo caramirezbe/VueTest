5f15d25d10e0b8b2b5a08878c076575af121253b
caramirezbe <camilo.ramirez.be@gmail.com>
2020-11-05 17:00:23 +1000

    Add required and banner

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,13 @@
 <template>
   <v-container>
+    <v-toolbar flat color="blue">
+      <v-toolbar-title>My Document</v-toolbar-title>
+    </v-toolbar>
     <v-form>
-
       <v-col cols="4">
         <v-text-field
             v-model="lastName"
+            :rules="[rules.required]"
             :counter="30"
             label="Last Name"
             required
@@ -14,6 +17,7 @@
       <v-col cols="4">
         <v-text-field
             v-model="firstName"
+            :rules="[rules.required]"
             :counter="30"
             label="First Name"
             required
@@ -23,6 +27,7 @@
       <v-col cols="4">
         <v-text-field
             v-model="email"
+            :rules="[rules.required]"
             :counter="30"
             label="Email"
             required
@@ -32,26 +37,35 @@
         <v-select
           :items="company"
           label="Company"
-          aria-required=""
+          :rules="[rules.required]"
         ></v-select>
       </v-col>
       <v-col cols="4">
         <v-text-field
           v-model="Pwd"
-          :counter="30"
+          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required, rules.mine]"
+          :type="show1 ? 'text' : 'password'"
+          @click:append="show1 = !show1" 
+          hint="At least 8 characters"
+          counter
           label="Password"
-          required
         ></v-text-field>
       </v-col>
       <v-col cols="4">
         <v-text-field
-            v-model="PwdAgain"
-            :counter="30"
-            label="Password Again"
-            required
+          v-model="PwdAgain"
+          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required]"
+          :type="show1 ? 'text' : 'password'"
+          @click:append="show1 = !show1" 
+          hint="At least 8 characters"
+          counter
+          label="Password Again"
+          required
         ></v-text-field>
       </v-col>  
-      <v-btn color="primary" &click="ShowInfo">Create User</v-btn>
+    <v-btn @click="showInfo()" color="primary">Create User</v-btn>
     </v-form>
 
     {{ lastName }}
@@ -72,12 +86,23 @@ export default {
       email:'',
       Pwd:'',
       PwdAgain:'',
-    }
-  }  
-}
+      show1: false,
+      password: 'Password',
+        
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 8 || 'Min 8 characters',
+        },  
 
 
+    } 
+  },
   
-
+  methods: {
+    showInfo(){
+      alert('User ' + this.lastName + ' ' + this.firstName + ' have created')
+    }
+  },  
+}
 
 </script>
