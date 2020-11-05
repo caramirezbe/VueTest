30683a7712f983c35740ba182501acf3c0bbf78e
caramirezbe <camilo.ramirez.be@gmail.com>
2020-11-05 14:17:26 +1000

    Complete form
    The form have alredy completed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,57 @@
 <template>
   <v-container>
     <v-form>
-      <v-text-field
-          v-model="lastName"
-          :counter="50"
-          label="Last Name"
-          required
-      ></v-text-field>
 
-      <v-btn color="primary">Create User</v-btn>
+      <v-col cols="4">
+        <v-text-field
+            v-model="lastName"
+            :counter="30"
+            label="Last Name"
+            required
+        ></v-text-field>
+      </v-col>
+
+      <v-col cols="4">
+        <v-text-field
+            v-model="firstName"
+            :counter="30"
+            label="First Name"
+            required
+        ></v-text-field>
+      </v-col>
+
+      <v-col cols="4">
+        <v-text-field
+            v-model="email"
+            :counter="30"
+            label="Email"
+            required
+        ></v-text-field>
+      </v-col>
+      <v-col cols="4">
+        <v-select
+          :items="company"
+          label="Company"
+          aria-required=""
+        ></v-select>
+      </v-col>
+      <v-col cols="4">
+        <v-text-field
+          v-model="Pwd"
+          :counter="30"
+          label="Password"
+          required
+        ></v-text-field>
+      </v-col>
+      <v-col cols="4">
+        <v-text-field
+            v-model="PwdAgain"
+            :counter="30"
+            label="Password Again"
+            required
+        ></v-text-field>
+      </v-col>  
+      <v-btn color="primary" &click="ShowInfo">Create User</v-btn>
     </v-form>
 
     {{ lastName }}
@@ -23,7 +66,18 @@ export default {
   data() {
     return {
       lastName: '',
+      firstName: '',
+      Email:'',   
+      company: ['CompanyA', 'CompanyB', 'CompanyC', 'CompanyD'],
+      email:'',
+      Pwd:'',
+      PwdAgain:'',
     }
-  }
+  }  
 }
+
+
+  
+
+
 </script>
