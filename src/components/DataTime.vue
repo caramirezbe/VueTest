faf7ff6420dc0e70a464db64ddfbd092c3ab0a8f
caramirezbe <camilo.ramirez.be@gmail.com>
2020-11-11 15:47:55 +1000

    Date problems solve
    Use v-picker to get the time.

--- src/components/DataTime.vue
+++ src/components/DataTime.vue
@@ -1,7 +1,7 @@
 <!-- - Read a date (day month year). (Bonus: use <v-date-picker>)
-    Show time between that day and today.
-    Show day of the week (write your own function).
-    Show is leap year (write your own function)
+    Show is leap year (write your own function) listo 
+    Show time between that day and today. 
+    Show day of the week (write your own function).listo 
     (Bonus) Show if it's the first week of the month. -->
 
 
@@ -13,18 +13,16 @@
     <v-form>
       <v-row>
         <v-col cols="4">
-          <v-date-picker
-              v-model="date1"
-          ></v-date-picker>
-          <v-date-picker
-              v-model="date2"
-          ></v-date-picker>
-          <v-btn @click="strDate()"
+          <v-date-picker v-model="picker1"></v-date-picker>
+          <v-date-picker v-model="picker2"></v-date-picker>
+          <v-btn @click="yearInformation()"
                  class="primary">Submit
           </v-btn>
         </v-col>
         <v-col cols="4">
-          {{ message }}
+         <v-alert> {{leapYear1Message}}</v-alert>
+         <v-alert> {{leapYear2Message}}</v-alert>
+         <v-alert> {{beteweenD}}</v-alert> 
         </v-col>
 
 
@@ -39,22 +37,53 @@ export default {
 
   data() {
     return {
-      date1: "2020-04-20",
-      date2: new Date().toISOString().substr(0, 10),
-      message: 'Hola',
+      picker1: new Date().toISOString().substr(0, 10),
+      picker2: new Date().toISOString().substr(0, 10),
+      leapYear1Message:'',
+      leapYear2Message:'',
+      weekday:['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
+      beteweenD:'',
     }
   },
 
   methods: {
 
-    strDate() {
-      const d = new Date(this.date1); // Convert string to date
-      // let iy = this.date1.replace(/-/gi, '').substr(0, 4)
-      // let im = this.date1.replace(/-/gi, '').substr(5)
-      // let id = this.date1.replace(/-/gi, '').substr(6, 8)
-      // console.log('year ' + iy)
-      // console.log('month ' + im)
-      // console.log('day ' + id)
+    yearInformation(){
+       const date1 = new Date(this.picker1)
+       const date2 = new Date(this.picker2) 
+       let day1 = this.weekday[date1.getDay()]
+       let day2 = this.weekday[date2.getDay()]
+       
+
+       if(this.leapYear(date1)){
+         this.leapYear1Message =  date1.getFullYear() + ' is a leap year and is ' + day1 
+       }
+       else{this.leapYear1Message = date1.getFullYear() + ' is not a leap year and is ' + day1 }
+       
+       if(this.leapYear(date2)){
+         this.leapYear2Message = date2.getFullYear() + ' is a leap year and is ' + day2 
+       }
+       else{this.leapYear2Message = date2.getFullYear() + ' is not a leap year and is ' + day2 }
+
+       if (date2 > date1){
+         this.beteweenD = 'The difference beteween this dates is: ' + Math.floor(date2-date1)/(1000*60*60*24)
+       }
+       else {this.beteweenD = 'The difference beteween this dates is: ' + Math.floor(date1-date2)/(1000*60*60*24)}
+    },
+
+   
+
+
+    leapYear(date) {
+      const year = date.getFullYear()
+      // return (year%4 === 0 && year%100 !==0 || year%400 === 0)
+      if (year%4 === 0 && year%100 !==0 || year%400 === 0){
+        return true
+        }
+      else{ 
+        return false
+      }
+     
     },
 
   }
